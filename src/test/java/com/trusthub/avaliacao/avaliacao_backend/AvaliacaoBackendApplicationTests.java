d0b06f36e28cc137850addf8863bc889db94c381
Luiz Fernando Povoa <luizfer1@yahoo.com.br>
2019-03-30 12:49:56 -0300

    Repositorio do codigo backend Java

---
+++ src/test/java/com/trusthub/avaliacao/avaliacao_backend/AvaliacaoBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trusthub.avaliacao.avaliacao_backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvaliacaoBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
